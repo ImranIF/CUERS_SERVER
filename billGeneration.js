cb1470b70e2aedccfae93c834fbc5a38837a28a5
ImranIF <imranfarid1980@outlook.com>
2023-05-18 11:52:05 +0600

    Fixed queries to Answer-script Examining and Question Moderation

--- billGeneration.js
+++ billGeneration.js
@@ -52,7 +52,6 @@ SELECT distinct eca.course_id as 'Course no',
        FROM ${tableName} eca where eca.evaluator_id=${evaluator_id} and semester_no = ${semester_no} and eca.sector_or_program like '${sector_or_program}' and eca.activity_type_id=${activity_type_id}
 group by eca.course_id, eca.sector_or_program, eca.evaluator_id, eca.semester_no;`;
         if (
-          activity_type_id === 3 ||
           activity_type_id === 6 ||
           (activity_type_id >= 8 && activity_type_id <= 15)
         ) {
@@ -119,7 +118,8 @@ SELECT distinct eca.course_id as 'Course no',
     (select real_bill FROM billCheck) AS 'টাকার পরিমাণ'
        FROM Evaluates_Course_Activity eca where eca.evaluator_id=${evaluator_id} and semester_no = ${semester_no} and eca.sector_or_program like '${sector_or_program}' and eca.activity_type_id=1
 group by eca.course_id, eca.sector_or_program, eca.evaluator_id, eca.semester_no;`;
-        } else if (activity_type_id === 3) {
+        } 
+        else if (activity_type_id === 3) {
           query = `WITH cntMembers AS (
     SELECT COUNT(*) AS count
     FROM Exam_Committee ec
@@ -139,7 +139,62 @@ SELECT distinct
        FROM
            Processes_Semester_Activity eca WHERE eca.evaluator_id=${evaluator_id} and semester_no = ${semester_no} and sector_or_program like '${sector_or_program}' and activity_type_id=3
 group by eca.sector_or_program, eca.evaluator_id, eca.semester_no;`;
-        } else if (activity_type_id === 5) {
+console.log(query)
+        } 
+        else if(activity_type_id === 4){
+          query = `WITH acFactor AS 
+          (SELECT DISTINCT factor FROM Activity ac WHERE ac.activity_type_id = ${activity_type_id}
+          AND ac.sector_or_program LIKE '${sector_or_program}%' ),
+          ecaQuantity AS ( SELECT DISTINCT quantity FROM Evaluates_Course_Activity eca
+          WHERE eca.activity_type_id = ${activity_type_id} AND eca.semester_no =${semester_no}
+          AND eca.evaluator_id = ${evaluator_id} AND eca.sector_or_program = '${sector_or_program}'
+          AND eca.factor = (SELECT factor from acFactor) ),
+              studentCheck AS (
+                  SELECT distinct ECA.course_id as "Course no",
+              sum(CASE WHEN factor like 'ছাত্রের সংখ্যা' or factor like 'পৃষ্ঠার সংখ্যা' THEN quantity ELSE 0 END) AS 'খাতা/ছাত্রের সংখ্যা/পৃষ্ঠার সংখ্যা',
+              sum(CASE WHEN factor like 'ঘণ্টা' THEN quantity ELSE 0 END) AS "কত ঘণ্টার পরীক্ষা",
+              sum(CASE WHEN factor like 'দিন' or factor like 'সদস্য সংখ্যা' or factor like 'পরীক্ষার সংখ্যা' THEN quantity ELSE 0 END) AS "মোট দিন/সদস্য সংখ্যা/পরীক্ষার সংখ্যা",
+              sum(CASE WHEN factor like 'অর্ধ/পূর্ণ' THEN quantity ELSE 0 END) AS 'অর্ধ/পূর্ণ পত্র'
+                  FROM Evaluates_Course_Activity ECA where ECA.evaluator_id= ${evaluator_id} and ECA.semester_no = ${semester_no}  and ECA.sector_or_program like '${sector_or_program}' and ECA.activity_type_id=${activity_type_id}
+          group by ECA.course_id, ECA.sector_or_program, ECA.evaluator_id, ECA.semester_no
+                  ),
+              calcCopyNoSNoP AS (
+                SELECT sum(CASE WHEN factor like 'ছাত্রের সংখ্যা' or factor like 'পৃষ্ঠার সংখ্যা' THEN quantity ELSE 0 END) AS 'খাতা/ছাত্রের সংখ্যা/পৃষ্ঠার সংখ্যা'
+                FROM Evaluates_Course_Activity ECA where ECA.evaluator_id= ${evaluator_id} and ECA.semester_no = ${semester_no}  and ECA.sector_or_program like '${sector_or_program}' and ECA.activity_type_id=${activity_type_id}
+          group by ECA.course_id, ECA.sector_or_program, ECA.evaluator_id, ECA.semester_no
+              ), calcDaysNoMNoE AS (
+                SELECT sum(CASE WHEN factor like 'দিন' or factor like 'সদস্য সংখ্যা' or factor like 'পরীক্ষার সংখ্যা' THEN quantity ELSE 0 END) AS "মোট দিন/সদস্য সংখ্যা/পরীক্ষার সংখ্যা"
+                FROM Evaluates_Course_Activity ECA where ECA.evaluator_id= ${evaluator_id} and ECA.semester_no = ${semester_no}  and ECA.sector_or_program like '${sector_or_program}' and ECA.activity_type_id=${activity_type_id}
+          group by ECA.course_id, ECA.sector_or_program, ECA.evaluator_id, ECA.semester_no
+              ), calcHalfFull AS (
+                SELECT sum(CASE WHEN factor like 'অর্ধ/পূর্ণ' THEN quantity ELSE 0 END) AS 'অর্ধ/পূর্ণ পত্র'
+                FROM Evaluates_Course_Activity ECA where ECA.evaluator_id= ${evaluator_id} and ECA.semester_no = ${semester_no}  and ECA.sector_or_program like '${sector_or_program}' and ECA.activity_type_id=${activity_type_id}
+          group by ECA.course_id, ECA.sector_or_program, ECA.evaluator_id, ECA.semester_no
+              ), calcHours AS (
+                SELECT sum(CASE WHEN factor like 'ঘণ্টা' THEN quantity ELSE 0 END) AS "কত ঘণ্টার পরীক্ষা"
+                FROM Evaluates_Course_Activity ECA where ECA.evaluator_id= ${evaluator_id} and ECA.semester_no = ${semester_no}  and ECA.sector_or_program like '${sector_or_program}' and ECA.activity_type_id=${activity_type_id}
+          group by ECA.course_id, ECA.sector_or_program, ECA.evaluator_id, ECA.semester_no
+              ), 
+           billCheck AS (
+              Select bill,
+              CASE WHEN (ac.quantity_final - ac.quantity_initial >= 10000  ) THEN bill
+               * (CASE WHEN ((SELECT * from calcCopyNoSNop)>0 ) THEN (SELECT * from calcCopyNoSNop) ELSE 1 END)
+                  * (CASE WHEN ((SELECT * from calcDaysNoMNoE)>0 ) THEN (SELECT * from calcDaysNoMNoE)  ELSE 1 END)
+                  * (CASE WHEN ((SELECT * from calcHours)>0 ) THEN (SELECT * from calcHours) ELSE 1 END)
+                  * (CASE WHEN ((SELECT * from calcHalfFull)>0 ) THEN (SELECT * from calcHalfFull) ELSE 1 END)
+                  ELSE bill * (SELECT * from calcCopyNoSNop) END AS real_bill FROM Activity ac INNER JOIN Evaluates_Course_Activity eca on eca.evaluator_id = ${evaluator_id}
+              AND eca.semester_no =  ${semester_no}
+              AND eca.activity_type_id = ${activity_type_id}
+              AND eca.activity_type_id = ac.activity_type_id AND eca.sector_or_program = '${sector_or_program}'
+              AND ac.sector_or_program LIKE '${sector_or_program}%' AND eca.factor = '${factor}'
+              AND ac.factor = (SELECT factor from acFactor)
+              AND (SELECT quantity from ecaQuantity) BETWEEN ac.quantity_initial AND ac.quantity_final )
+                  SELECT studentCheck.*, (select real_bill FROM billCheck) AS 'টাকার পরিমাণ', (Select bill from billCheck) as 'Initial bill'
+                  FROM studentCheck;`;
+            // console.log(query)
+        }
+        
+        else if (activity_type_id === 5) {
           query = `with daysCheck as (select distinct quantity from Evaluates_Course_Activity eca
             where activity_type_id = 5 and eca.factor = 'দিন' ),
      hoursCheck as (select distinct quantity from Evaluates_Course_Activity eca
@@ -166,7 +221,7 @@ SELECT distinct eca.course_id as 'Course no',
        FROM Evaluates_Course_Activity eca where eca.evaluator_id=${evaluator_id} and semester_no = ${semester_no} and sector_or_program like 'ব্যবহারিক' and activity_type_id=5
 group by eca.course_id, eca.sector_or_program, eca.evaluator_id, eca.semester_no;`;
         }
-        console.log("query:", query);
+          // console.log("query:", query);
 
         conn.query(query, (err, results) => {
           if (err) {
@@ -192,7 +247,7 @@ group by eca.course_id, eca.sector_or_program, eca.evaluator_id, eca.semester_no
     }
   );
   return Promise.all(promises).then(() => {
-    console.log("Begula", activityThings);
+    // console.log("Begula", activityThings);
     return activityThings;
   });
 }
